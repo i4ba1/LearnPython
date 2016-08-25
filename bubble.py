e29d675c789600ba9397b460fec361d013012764
i4ba1 <muhamad.iqbal1987@gmail.com>
2016-08-25 18:49:21 +0700

    Finish bubble sort

--- bubble.py
+++ bubble.py
@@ -1,13 +1,17 @@
+def bubble_sort(number_list):
+    for i in range(0, len(number_list)-1, 1):
+        for j in range(1, len(number_list)-i, 1):
+            if(number_list[j] < number_list[j-1]):
+                temp = number_list[j]
+                number_list[j] = number_list[j-1]
+                number_list[j-1] = temp
+    print(number_list)
+    return number_list
 
-    def bubble_sort(number_list):
-        
-        for i in range(0, len(number_list)-1, 1):
-            for j in range(0, len(object)):
-                if(number_list[j] > number_list[j+1]):
-                    temp = number_list[j]
-                    number_list[j] = number_list[j+1]
-                    number_list[j+1] = temp
-        return number_list
 
-        
+def main():        
+    number_list = [13,5,7,1,0]
+    bubble_sort(number_list)    
+    print(number_list)
     
+main()
